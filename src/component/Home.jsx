cc3090bc1ae58437e3bf0120a2c22d69f9cfac9e
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2024-02-28 22:24:40 +0600

    first commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import img from '../assets/9.dfffbb66f86faeacf924.png'
 function Home() {
   return (
-    <div className='bg-zinc-800 h-screen'>
-       <div className='flex justify-center items-center  flex-col'>
-       <img src={img} alt="" className='md:h-[400px] md:w-[600px] h-[200px] w-[200px]  sm:h-[350px] sm:w-[400px] md:mt-[15px]  mt-[200px]  lg:mt-3' />
+    <div className='bg-zinc-800 '>
+       <div className='flex justify-center items-center  flex-col lg:min-h-[80vh] min-h-[100vh]'>
+       <img src={img} alt="" className='md:h-[400px] md:w-[600px] h-[200px] w-[200px]  sm:h-[350px] sm:w-[400px] ' />
       <div className='md:text-3xl text-xl font-bold flex flex-col text-white text-center mt-4 sm:mt-0'>
       <p>Mission To 1 Billion</p>
        <p>Market Cap</p>
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
         setOpen(!open)
     }
   return (
-   <div className='relative'>
+   <div className='relative '>
     
      <div className='flex items-center lg:mx-32 my-2  w-full fixed ml-8'>
         <div className='font-bold md:text-5xl text-white text-xl sm:text-2xl'>icon</div>
@@ -17,11 +17,15 @@ function Nav() {
             </div>
         </div>
         <div>
-            <button className='bg-green-300 md:px-12 md:py-4 py-2 px-4 border-black md:border-8 border-2 lg:mr-60 mr-12 sm:text-xl sm:font-bold'>Telegram</button>
+            <button className='bg-green-300 px-2 lg:px-8 md:py-4 py-2 border-black md:border-8 border-2 lg:mr-60 mr-12 sm:text-xl sm:font-bold'>Telegram</button>
         </div>
-    <button className='absolute bg-slate-200 md:m-6 md:p-5 top-12 md:left-[-50px]  lg:hidden p-2 sm:font-bold sm:text-xl' onClick={isOpen}>menu</button>
-    <div className={`absolute h-[70vh] w-full bg-green-400 lg:hidden top-52 opacity-45 left-[-35px] transition-all ${open?'block':'translate-y-[100vh]'}`}>
-       <div className='grid justify-center mt-64'>
+    <button className='absolute bg-slate-200 md:m-6 md:p-5 top-12 md:left-[-50px]  lg:hidden p-2 sm:font-bold sm:text-xl z-50' onClick={isOpen}>menu</button>
+    <div className={`fixed top-0 left-0 w-full h-full ${open&&'bg-black'}  bg-opacity-75`}>
+
+    </div>
+
+    <div className={`absolute h-[70vh] w-full bg-white text-black lg:hidden top-52 opacity-45 left-[-35px] transition-all ${open?'block':'translate-y-[100vh]'}`}>
+       <div className='grid justify-center mt-64 '>
        <p className='text-3xl font-bold'>Home</p>
        <p className='text-3xl font-bold'>chart</p>
        </div>
